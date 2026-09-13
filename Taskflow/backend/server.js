dfeb37736bbb01097efd716d5ec5a91767abec92
ishan09ahmad <mdishanahmad@gmail.com>
2026-09-14 01:58:03 +0530

    update server.js

--- Taskflow/backend/server.js
+++ Taskflow/backend/server.js
@@ -2,8 +2,10 @@ import "dotenv/config"
 import app from "./src/app.js";
 import connectDB from "./src/config/mongodb.js";
 await connectDB();
-const port=process.env.PORT;
+// const port=process.env.PORT;
 
 // app.listen(port, () => {
 //   console.log("server is running on port:", port);
 // });
+
+export default app;
